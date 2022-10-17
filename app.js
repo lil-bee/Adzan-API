178f99c671c1081872e742132e2d0c5dbfdd218a
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2022-10-17 10:45:03 +0700

    permission geolocation

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+function successCallback(position) {
+    console.log(position);
+}
+
+function errorCallback() {
+    alert('posisi tidak dapat diakses');
+}
+
+
+
+function userLocation() {
+    if (!navigator.geolocation) {
+        alert("browser anda tidak mendukung geolocation")
+    } else {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+    }
+}
+
 function index() {
     let app = document.getElementById('app');
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Waktu Sholat';
 
     app.appendChild(h3);
+
+    userLocation();
 }
 index();
